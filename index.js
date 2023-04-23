28c1a2d0cf395a2a1e314614c866bbaf67a8837c
Asier González <asiergontz8@gmail.com>
2023-04-23 12:32:58 +0200

    resolving old labs

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
 
-  max() {}
+    this.items.sort(function(a,b) {
+      if (a<b) return -1
+      if (a>b) return 1
+      if (a=b) return 0
+  })
+  return this.items, this.length
 
-  min() {}
+  }
+
+  get(pos) {
+    if (pos<=this.items.length) {
+      return this.items[pos]
+
+    } else {
+      throw new Error('OutOfBounds');
+    }
+
+  }
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items)
+
+    }
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items)
+
+    }
+  }
 
   sum() {}
 
